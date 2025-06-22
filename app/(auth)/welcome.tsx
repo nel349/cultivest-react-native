b88762bf9f73a982cf3100945a1f40d6126b5c38
Norman Lopez <noell.lpz@gmail.com>
2025-06-21 20:55:03 -0700

    Updated welcome.tsx

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -133,7 +133,7 @@ const styles = StyleSheet.create({
   decorationContainer: {
     position: 'absolute',
     width: '100%',
-    height: '100%',
+    height: '80%',
   },
   plantDecor: {
     position: 'absolute',
